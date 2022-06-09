2892c52eade7c14f11787974889b93b8bf995968
wjdqls0303 <wjdqls0303@gmail.com>
2022-06-09 09:15:28 +0900

    6/9

--- Assets/Scripts/CameraPosition.cs
+++ Assets/Scripts/CameraPosition.cs
@@ -4,24 +4,143 @@ using UnityEngine;
 
 public class CameraPosition : MonoBehaviour
 {
-    public Transform playerCamTrans;
+    [Header("카메라 기본 속성")]
+    //카메라 위치 캐싱 준비
+    private Transform cameraTransform = null;
 
-    public float xDelta = 10f;
-    public float yDelta = 10f;
+    //target
+    public GameObject objTarget = null;
 
-    float rotationX;
-    float rotationY;
+    //player transform 캐싱
+    private Transform objTargetTransform = null;
 
-    void Update()
+    //카메라가 3가지야
+    public enum CameraTypeState { First, Second, Third }
+
+    //카메라 기본 3인칭 
+    public CameraTypeState cameraState = CameraTypeState.Third;
+
+    [Header("3인칭 카메라")]
+    //떨어진 거리
+    public float distance = 6.0f;
+
+    //추가 높이
+    public float height = 5f;
+
+    //smooth time
+    public float heightDamp = 2f;
+    public float rotationDamping = 3f;
+
+    [Header("2인칭카메라")]
+    public float rotationSpd = 10f;
+
+    [Header("1인칭카메라")]
+    //마우스 카메라 조절 디테일 좌표
+    public float detailX = 5f;
+    public float detailY = 5f;
+
+    //마우스 회전 값
+    private float rotationX = 0f;
+    private float rotationY = 0f;
+
+    //캐싱
+    public Transform posFirstTarget = null;
+
+    void FirstCamera()
     {
-        float x = Input.GetAxis("Mouse X");
-        float y = Input.GetAxis("Mouse Y");
-        rotationX = rotationX + x * xDelta;
+        //마우스 좌표 가져오기
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
 
-        rotationY = rotationY + y * yDelta;
-        rotationY = Mathf.Clamp(rotationY, -40, 80);
-        // eulerangle와 roatation차이 
-        transform.rotation = Quaternion.Euler(new Vector3(-rotationY, rotationX, 0));
-        transform.position = playerCamTrans.position;
+        rotationX = cameraTransform.localEulerAngles.y + mouseX * detailX;
+        rotationX = (rotationX > 180.0f) ? rotationX - 360.0f : rotationX;
+        rotationY = rotationY + mouseY * detailY;
+        rotationY = (rotationY > 180.0f) ? rotationY - 360.0f : rotationY;
+
+        cameraTransform.localEulerAngles = new Vector3(-rotationY, rotationX, 0f);
+        cameraTransform.position = posFirstTarget.position;
+    }
+
+    /// <summary>
+    /// 2인칭 카메라 조작
+    /// </summary>
+    void SecondCamera()
+    {
+        cameraTransform.RotateAround(objTargetTransform.position, Vector3.up, rotationSpd * Time.deltaTime);
+
+        cameraTransform.LookAt(objTargetTransform.position);
+    }
+
+    private void LateUpdate()
+    {
+        if (objTarget == null)
+        {
+            return;
+        }
+
+        if (objTargetTransform == null)
+        {
+            objTargetTransform = objTarget.transform;
+        }
+
+        switch (cameraState)
+        {
+            case CameraTypeState.Third:
+                ThirdCamera();
+                break;
+            case CameraTypeState.Second:
+                SecondCamera();
+                break;
+            case CameraTypeState.First:
+                FirstCamera();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void Start()
+    {
+        cameraTransform = GetComponent<Transform>();
+
+        //타겟이 있냐?
+        if (objTarget != null)
+        {
+            objTargetTransform = objTarget.transform;
+        }
+    }
+
+    /// <summary>
+    /// 3인칭 카메라 기본 동작 함수
+    /// </summary>
+    void ThirdCamera()
+    {
+        //현재 타겟 y축 각도 값
+        float objTargetRotationAngle = objTargetTransform.eulerAngles.y;
+
+        //현재 타겟 높이 + 카메라가 위치한 높이 추가 높이
+        float objHeight = objTargetTransform.position.y + height;
+
+        //현재 각도 높이
+        float nowRotationAngle = cameraTransform.eulerAngles.y;
+        float nowHeight = cameraTransform.position.y;
+
+        //현재 각도에 대한 DAMP
+        nowRotationAngle = Mathf.LerpAngle(nowRotationAngle, objTargetRotationAngle, rotationDamping * Time.deltaTime);
+
+        //현재 높이에 대한 DAMP
+        nowHeight = Mathf.Lerp(nowHeight, objHeight, heightDamp * Time.deltaTime);
+
+        //유니티 각도로 변경
+        Quaternion nowRotation = Quaternion.Euler(0f, nowRotationAngle, 0f);
+
+        //카메라 위치 포지션 이동
+        cameraTransform.position = objTargetTransform.position;
+        cameraTransform.position -= nowRotation * Vector3.forward * distance;
+
+        //최종이동
+        cameraTransform.position = new Vector3(cameraTransform.position.x, nowHeight, cameraTransform.position.z);
+
+        cameraTransform.LookAt(objTargetTransform);
     }
 }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,44 +4,109 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float PlayerSpd = 10f;
-    public float PlayerGrv = 9.8f;
-    public float rotSpd = 10f;
+    [Header("속성")]
+    //캐릭터 이동속도 설정
+    [Tooltip("캐릭터 이동속도 설정")]
+    public float moveSpd = 5.0f;
+    //캐릭터 이동속도 설정
+    public float runMoveSpd = 10.0f;
+    //캐릭터 이동방향 회전 속도 설정
+    public float DirectionRotateSpd = 100.0f;
+    //캐릭터 몸을 움직이는 회전 속도 설정
+    public float BodyRotateSpd = 3.5f;
+    //캐릭터 속도 변경 증가 값
+    [Range(0.1f, 50.0f)]
+    public float VelocityChangeSpd = 0.1f;
+    //캐릭터 현재 이동 속도 설정 초기값
+    private Vector3 CurrentVelocitySpd = Vector3.zero;
+    //캐릭터 현재 이동 방향 초기값 설정
     private Vector3 MoveDirect = Vector3.zero;
+    //CharacterController 캐싱 준비
+    private CharacterController characterCtrl = null;
+    //캐릭터 CollisionFlags 초기값 설정
+    private CollisionFlags collisionflages = CollisionFlags.None;
+    [Header("애니메이션 속성")]
+    public AnimationClip animationClipIdle = null;
+    public AnimationClip animationClipWalk = null;
+    public AnimationClip animationClipRun = null;
+    public AnimationClip animationClipAtk_1 = null;
+    public AnimationClip animationClipAtk_2 = null;
+    public AnimationClip animationClipAtk_3 = null;
+    public AnimationClip animationClipAtk_4 = null;
 
-    private Vector3 moveDir;
+    //animation component 캐싱 준비
+    private Animation animationPlayer = null;
+    //캐릭터 상태
+    public enum PlayerState { None, Idle, Walk, Run, Atk }
 
-    private CharacterController characterController;
+    [Header("캐릭터 상태")]
+    public PlayerState playerState = PlayerState.None;
 
+    //공격 상태
+    public enum PlayerAttackState { atkStep_1, atkStep_2, atkStep_3, atkStep_4 }
+
+    [Header("공격 상태")]
+    public PlayerAttackState playerAttackState = PlayerAttackState.atkStep_1;
+
+    //다음 연계 공격 활성화 여부를 확인하기 위해 flag 설정
+    public bool flagNextAttack = false;
+
+    [Header("전투관련")]
+    //공격할 때만 켜지게
+    public TrailRenderer AtkTrailRenderer = null;
+
+    //무기에 있는 콜라이더 캐싱
+    public CapsuleCollider AtkCapsuleCollider = null;
+
+    [Header("스킬관련")]
+    public AnimationClip skillAnimClip = null;
+    public GameObject skillEffect = null;
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        //CharacterController 캐싱
+        characterCtrl = GetComponent<CharacterController>();
+        //animation 캐싱
+        animationPlayer = GetComponent<Animation>();
+        //자동 재생 off
+        animationPlayer.playAutomatically = false;
+        //현재 재생중인 애니메이션을 STOP
+        animationPlayer.Stop();
+        //상태값을 기본값
+        playerState = PlayerState.Idle;
+        //애니메이션 상태에 클립 연결
+        animationPlayer[animationClipIdle.name].wrapMode = WrapMode.Loop;
+        animationPlayer[animationClipWalk.name].wrapMode = WrapMode.Loop;
+        animationPlayer[animationClipRun.name].wrapMode = WrapMode.Loop;
+        animationPlayer[animationClipAtk_1.name].wrapMode = WrapMode.Once;
+        animationPlayer[animationClipAtk_2.name].wrapMode = WrapMode.Once;
+        animationPlayer[animationClipAtk_3.name].wrapMode = WrapMode.Once;
+        animationPlayer[animationClipAtk_4.name].wrapMode = WrapMode.Once;
+
+        //animationPlayer[skillAnimClip.name].wrapMode = WrapMode.Once;   
+
+        SetAnimationEvent(animationClipAtk_1, "OnPlayerAttackFinshed");
+        SetAnimationEvent(animationClipAtk_2, "OnPlayerAttackFinshed");
+        SetAnimationEvent(animationClipAtk_3, "OnPlayerAttackFinshed");
+        SetAnimationEvent(animationClipAtk_4, "OnPlayerAttackFinshed");
+
+        //SetAnimationEvent(skillAnimClip, "OnSkillAnimFinished");
     }
 
     void Update()
     {
-        Moving();
+        Move();
+        gravitySet();
+        Debug.Log(GetVelocitySpd());
+        BodyDirectionChange();
+        AnimationClipCtrl();
+        ckAnimationState();
+        InputAttackCtrl();
     }
-
-    void Moving()
+    /// <summary>
+    /// 캐릭터 이동 함수
+    /// </summary>
+    void Move()
     {
-        //Transform cameraTransform = Camera.main.transform;
-        //Vector3 forward = cameraTransform.TransformDirection(Vector3.forward);
-
-        //float horizontal = Input.GetAxisRaw("Horizontal");
-        //float vertical = Input.GetAxisRaw("Vertical");
-
-        //moveDir = new Vector3(horizontal, 0, vertical);
-
-        //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(moveDir), rotSpd * Time.deltaTime);
-
-        //moveDir *= PlayerSpd;
-
-        //moveDir.y += Physics.gravity.y * Time.deltaTime;
-
-        //characterController.Move(moveDir * Time.deltaTime);
-
-        //transform.position += new Vector3(horizontal, 0, vertical) * PlayerSpd * Time.deltaTime ;
         //메인카메라 Transform
         Transform cameraTransform = Camera.main.transform;
         //메인카메라가 바라보는 방향이 월드상에서 어떤 방향인가
@@ -64,9 +129,288 @@ public class PlayerMove : MonoBehaviour
         {
             spd = runMoveSpd;
         }
+
+        Vector3 gravitySpd = new Vector3(0, verticalSpd, 0);
+
         //이동하는 프레임 양
-        Vector3 moveAmount = (MoveDirect * spd * Time.deltaTime);
+        Vector3 moveAmount = (MoveDirect * spd * Time.deltaTime) + gravitySpd;
         //실제 이동
         collisionflages = characterCtrl.Move(moveAmount);
+    }
+
+    float GetVelocitySpd()
+    {
+        if (characterCtrl.velocity == Vector3.zero)
+        {
+            CurrentVelocitySpd = Vector3.zero;
+        }
+        else
+        {
+            Vector3 retVelocitySpd = characterCtrl.velocity;
+            retVelocitySpd.y = 0;
+            CurrentVelocitySpd = Vector3.Lerp(CurrentVelocitySpd, retVelocitySpd, VelocityChangeSpd * Time.fixedDeltaTime);
+        }
+
+        return CurrentVelocitySpd.magnitude;
+    }
+
+    /// <summary>
+    /// 몸통 방향 함수 작성
+    /// </summary>
+    void BodyDirectionChange()
+    {
+        //움직이고 있는가?
+        if (GetVelocitySpd() > 0.0f)
+        {
+            //캐릭터 몸통이 바라볼 전방은? 캐릭터 속도 방향
+            Vector3 newForward = characterCtrl.velocity;
+            newForward.y = 0.0f;
+            //캐릭터를 전방으로 방향을 설정한다.
+            transform.forward = Vector3.Lerp(transform.forward, newForward, BodyRotateSpd);
+        }
+    }
+
+    //private void OnGUI()
+    //{
+    //    if (characterCtrl != null && characterCtrl.velocity != Vector3.zero)
+    //    {
+    //        var labelStyle = new GUIStyle();
+    //        labelStyle.fontSize = 30;
+    //        labelStyle.normal.textColor = Color.white;
+    //        //현재 속도
+    //        float _getVelocity = GetVelocitySpd();
+    //        GUILayout.Label("현재속도 : " + _getVelocity.ToString(), labelStyle);
+    //        //현재 캐릭터 방향
+    //        GUILayout.Label("현재 방향 : " + characterCtrl.velocity.ToString(), labelStyle);
+    //        //현재 캐릭터 속도
+    //        GUILayout.Label("캐릭터 속도 : " + CurrentVelocitySpd.magnitude.ToString(), labelStyle);
+    //    }
+    //}
+
+    /// <summary>
+    /// 애니메이션 재생 함수
+    /// </summary>
+    /// <param name="clip">애니메이션 클립</param>
+    void playAnimationByClip(AnimationClip clip)
+    {
+        //animationPlayer.clip = clip;
+        //animationPlayer.GetClip(clip.name);
+        animationPlayer.CrossFade(clip.name);
+    }
+
+    /// <summary>
+    /// 플레이어 상태에 맞춘 애니메이션을 재생
+    /// </summary>
+    void AnimationClipCtrl()
+    {
+        switch (playerState)
+        {
+            case PlayerState.Idle:
+                playAnimationByClip(animationClipIdle);
+                break;
+            case PlayerState.Walk:
+                playAnimationByClip(animationClipWalk);
+                break;
+            case PlayerState.Run:
+                playAnimationByClip(animationClipRun);
+                break;
+            case PlayerState.Atk:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 상태에 따른 변경해주는 함수
+    /// </summary>
+    void ckAnimationState()
+    {
+        float nowSpd = GetVelocitySpd();
+        switch (playerState)
+        {
+            case PlayerState.Idle:
+                if (nowSpd > 0.0f)
+                {
+                    playerState = PlayerState.Walk;
+                }
+                break;
+            case PlayerState.Walk:
+                if (nowSpd > 4.5f)
+                {
+                    playerState = PlayerState.Run;
+                }
+                else if (nowSpd < 0.1f)
+                {
+                    playerState = PlayerState.Idle;
+                }
+                break;
+            case PlayerState.Run:
+                if (nowSpd < 4.5f)
+                {
+                    playerState = PlayerState.Walk;
+                }
+                if (nowSpd < 0.1f)
+                {
+                    playerState = PlayerState.Idle;
+                }
+                break;
+            case PlayerState.Atk:
+                AtkAnimationCtrl();
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 공격 버튼 체크 함수
+    /// </summary>
+    void InputAttackCtrl()
+    {
+        //마우스 클릭 감지
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            //플레이어가 공격상태
+            if (playerState != PlayerState.Atk)
+            {
+                //플레이어가 공격상태가 아니면 공격 상태로 만든다
+                playerState = PlayerState.Atk;
+                //공격상태 초기화
+                playerAttackState = PlayerAttackState.atkStep_1;
+            }
+            else
+            {
+                //플레이어 상태가 공격일 때 
+                //공격 상태에 따른 분류
+                switch (playerAttackState)
+                {
+                    case PlayerAttackState.atkStep_1:
+                        if (animationPlayer[animationClipAtk_2.name].normalizedTime > 0.1f)
+                        {
+                            flagNextAttack = true;
+                        }
+                        break;
+                    case PlayerAttackState.atkStep_2:
+                        if (animationPlayer[animationClipAtk_3.name].normalizedTime > 0.1f)
+                        {
+                            flagNextAttack = true;
+                        }
+                        break;
+                    case PlayerAttackState.atkStep_3:
+                        if (animationPlayer[animationClipAtk_4.name].normalizedTime > 0.1f)
+                        {
+                            flagNextAttack = true;
+                        }
+                        break;
+                    case PlayerAttackState.atkStep_4:
+                        if (animationPlayer[animationClipAtk_1.name].normalizedTime > 0.1f)
+                        {
+                            flagNextAttack = true;
+                        }
+                        break;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// CallBack 공격 애니메이션 재생이 끝나면 호출 되는 애니메이션 이벤트 함수
+    /// </summary>
+    void OnPlayerAttackFinshed()
+    {
+        //만약에 flagNextAttack이 true면
+        if (flagNextAttack == true)
+        {
+            //flagNextAttkack 초기화
+            flagNextAttack = false;
+
+            //현재 공격 애니메이션 상태에 따른 다음 애니메이션 상태값을 넣기
+            switch (playerAttackState)
+            {
+                case PlayerAttackState.atkStep_1:
+                    playerAttackState = PlayerAttackState.atkStep_2;
+                    break;
+                case PlayerAttackState.atkStep_2:
+                    playerAttackState = PlayerAttackState.atkStep_3;
+                    break;
+                case PlayerAttackState.atkStep_3:
+                    playerAttackState = PlayerAttackState.atkStep_4;
+                    break;
+                case PlayerAttackState.atkStep_4:
+                    playerAttackState = PlayerAttackState.atkStep_1;
+                    break;
+            }
+        }
+        else
+        {
+            playerState = PlayerState.Idle;
+
+            playerAttackState = PlayerAttackState.atkStep_1;
+        }
+    }
+
+    /// <summary>
+    /// 애니메이션 클립 재생이 끝날때 애니메이션 이벤트 함수를 호출
+    /// </summary>
+    /// <param name="clip">애니메이션 클립</param>
+    /// <param name="funcName">이벤트 함수</param>
+    void SetAnimationEvent(AnimationClip clip, string funcName)
+    {
+        //새로운 이벤트 선언
+        AnimationEvent newAnimationEvent = new AnimationEvent();
+
+        //해당 이벤트의 호출은 funcName
+        newAnimationEvent.functionName = funcName;
+        //newAnimationEvent.time = clip.length - 0.1f;
+        clip.AddEvent(newAnimationEvent);
+    }
+
+    /// <summary>
+    /// 공격 애니메이션 재생
+    /// </summary>
+    void AtkAnimationCtrl()
+    {
+        //만약 공격상태가?
+        switch (playerAttackState)
+        {
+            case PlayerAttackState.atkStep_1:
+                playAnimationByClip(animationClipAtk_1);
+                break;
+            case PlayerAttackState.atkStep_2:
+                playAnimationByClip(animationClipAtk_2);
+                break;
+            case PlayerAttackState.atkStep_3:
+                playAnimationByClip(animationClipAtk_3);
+                break;
+            case PlayerAttackState.atkStep_4:
+                playAnimationByClip(animationClipAtk_4);
+                break;
+        }
+    }
+    private float gravity = 9.8f;
+    private float verticalSpd = 0f;
+    void gravitySet()
+    {
+        if ((collisionflages & CollisionFlags.CollidedBelow) != 0)
+        {
+            verticalSpd = 0f;
+        }
+        else
+        {
+            verticalSpd -= gravity + Time.deltaTime;
+        }
+    }
+
+    //스킬 애니메이션 재생이 끝났을 때 이벤트
+    void OnSkillAnimFinished()
+    {
+        //현재 캐릭터 위치 저장
+        Vector3 pos = transform.position;
+
+        //캐릭터 앞 방향 2.0정도 떨어진 거리
+        pos += transform.forward * 2f;
+
+        //그 위치에 스킬 이펙트를 붙인다.
+        Instantiate(skillEffect, pos, Quaternion.identity);
+
+        //끝났으면 대기 상태로 둔다.
+        playerState = PlayerState.Idle;
     }
 }
